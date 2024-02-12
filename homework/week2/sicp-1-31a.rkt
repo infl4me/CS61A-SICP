13895b03b1e1dd3d99789ea369f591751e56bf8d
udf <hellpl4y@gmail.com>
2024-02-12 12:24:45 +0500

    [hw2] add sicp 131a

---
+++ homework/week2/sicp-1-31a.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (identity x) x)
+
+(define (product term a b)
+  (if (> a b)
+    1
+    (* a (product term (+ a 1) b))))
+
+(define (factorial n)
+  (product identity 1 n))
+
+(factorial 1) ; 1
+(factorial 3) ; 6
+(factorial 5) ; 120
+(factorial 8) ; 40320
