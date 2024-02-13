24906d268f3a484fa1f0e122cbeede12b0fefb5a
Sergey P <hellpl4y@gmail.com>
2024-02-13 16:06:47 +0500

    [hw2] add sicp 146

--- homework/week2/sicp-1-46.rkt
+++ homework/week2/sicp-1-46.rkt
@@ -2,11 +2,11 @@
 
 (define (square x) (* x x))
 
-(define (iterative-improve good-enough? improve-guess)
+(define (iterative-improve good-enough? improve)
   (define (iterate guess)
     (if (good-enough? guess)
         guess
-        (iterate (improve-guess guess))))
+        (iterate (improve guess))))
 
   iterate)
 
@@ -21,3 +21,13 @@
 
 (sqrt 9) ; 3.00009155413138
 (sqrt (+ 100 37)) ; 11.704699917758145
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  ((iterative-improve (lambda (guesses)
+			(< (abs (- (first guesses) (last guesses))) tolerance))
+		      (lambda (guesses)
+			(list (last guesses) (f (last guesses)))))
+   (list first-guess (f first-guess))))
+
+(fixed-point cos 1.0) ; .7390822985224023
