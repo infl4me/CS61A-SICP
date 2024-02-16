e16b17f129569142a8ce40ed7b19830412b02c9b
udf <hellpl4y@gmail.com>
2024-02-16 07:04:59 +0500

    [lab3] complete lab

--- labs/week3.rkt
+++ labs/week3.rkt
@@ -70,3 +70,40 @@
   )
 
 (count-change2 100) ; 292
+
+
+
+
+
+; 4. Many Scheme procedures require a certain type of argument. For example, the arithmetic procedures
+; only work if given numeric arguments. If given a non-number, an error results.
+; Suppose we want to write safe versions of procedures, that can check if the argument is okay, and either
+; call the underlying procedure or return #f for a bad argument instead of giving an error. (We’ll restrict our
+; attention to procedures that take a single argument.)
+
+; Write type-check. Its arguments are a function, a type-checking predicate that returns #t if and only if
+; the datum is a legal argument to the function, and the datum.
+
+(define (type-check fn predicate arg)
+  (if (predicate arg)
+      (fn arg)
+      #f))
+
+(type-check sqrt number? 'hello) ; #f
+(type-check sqrt number? 4) ; 2
+
+; 5. We really don’t want to have to use type-check explicitly every time. Instead, we’d like to be able to
+; use a safe-sqrt procedure:
+; > (safe-sqrt ’hello)
+; #f
+
+; Don’t write safe-sqrt! Instead, write a procedure make-safe that you can use this way:
+; > (define safe-sqrt (make-safe sqrt number?))
+; It should take two arguments, a function and a type-checking predicate, and return a new function that
+; returns #f if its argument doesn’t satisfy the predicate.
+
+(define (make-safe fn predicate) (lambda (x) (if (predicate x) (fn x) #f)))
+(define safe-sqrt (make-safe sqrt number?))
+
+(safe-sqrt 'hello) ; #f
+(safe-sqrt 4) ; 2
